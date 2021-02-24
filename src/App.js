cbaee4a3c05aedce975acd0d45c2c86e798f89b7
Cody Mullen <mullen.cody@yahoo.com>
2021-02-23 21:54:23 -0800

    adjusting footer to be in contact container

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import NavBar from './components/NavBar';
 import About from './components/About';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
-import Footer from './components/Footer';
+// import Footer from './components/Footer';
 
 function App() {
   return (
@@ -15,7 +15,7 @@ function App() {
       <About />
       <Projects />
       <Contact />
-      <Footer />
+      {/* <Footer /> */}
     </div>
   );
 }
--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -2,6 +2,11 @@ import React, { useEffect } from 'react';
 import './style.css';
 import Aos from 'aos';
 import 'aos/dist/aos.css';
+import { BrowserRouter as Router, } from 'react-router-dom';
+import { AiFillLinkedin } from 'react-icons/ai';
+import { AiFillInstagram } from 'react-icons/ai';
+import { AiFillGithub } from 'react-icons/ai';
+import { AiFillFileText } from 'react-icons/ai';
 
 const Contact = () => {
   useEffect(() => {
@@ -24,6 +29,16 @@ const Contact = () => {
           Send!
         </button>
       </div>
+      <Router>
+        <div className='footContainer'>
+          <div className='contactLinks'>
+            <a className='footLogo' href='https://www.linkedin.com/in/codymu/' target='_blank' rel='noreferrer'><AiFillLinkedin id='linkedInLogo' />LinkedIn</a>
+            <a className='footLogo' href='https://github.com/codymu45' target='_blank' rel='noreferrer'><AiFillGithub id='githubLogo' />Github</a>
+            <a className='footLogo' href='https://www.instagram.com/c_murren/' target='_blank' rel='noreferrer'><AiFillInstagram id='instagramLogo' />Instagram</a> 
+            <a className='footLogo' href='https://docs.google.com/document/d/1pt4XwT198rftlTEp9oAScGZCWY0UyKX9_7We-1WaW10/edit' target='_blank' rel='noreferrer'><AiFillFileText id='resumeLogo' />Resume</a> 
+          </div>
+        </div>
+      </Router>
     </div>
   )
 }
