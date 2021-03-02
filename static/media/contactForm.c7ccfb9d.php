4dc12e3d2472733fc73812d13143d0fd1b8f081c
Cody Mullen <mullen.cody@yahoo.com>
2021-03-02 09:53:45 -0800

    Updates

---
+++ static/media/contactForm.c7ccfb9d.php
@@ -0,0 +1,14 @@
+<?php
+
+if (isset($_POST['contactSubmit'])) {
+  $name = $_POST['inputName'];
+  $email = $_POST['inputEmail'];
+  $message = $_POST['inputMessage'];
+
+  $mailTo = "mullen.cody@yahoo.com";
+  $headers = "From: ".$email;
+  $txt = "You have a new message from ".$name.".\n\n".$message;
+
+  mail($mailTo, $headers, $txt);
+  header("Location: index.php?mailsent");
+}
